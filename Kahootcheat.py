7367fc4a0afd7cd1411f22e27ae994fe6c80bc8b
mbisch11 <87216223+mbisch11@users.noreply.github.com>
2021-10-24 13:21:15 -0400

    Update Kahootcheat.py

--- Kahootcheat.py
+++ Kahootcheat.py
@@ -4,11 +4,11 @@ import json
 gameid = input("Enter game id: ")
 url = "https://play.kahoot.it/rest/kahoots/" + gameid
 q = json.loads(lmao.urlopen(url).read())["questions"]
-colors_list = ["Top Reft", "Top Right", "Bottom Left", "Bottom Right"]
+position_list = ["Top Reft", "Top Right", "Bottom Left", "Bottom Right"]
 
 for index, slide in enumerate(q):
     if slide.get("type") == "quiz":
         for i in range(len(slide.get("choices"))):
             if slide["choices"][i]["correct"] == True:
                 print("Question number: {}\n{}\n{}\n".format(
-                    index + 1, slide["choices"][i].get("answer"), colors_list[i]))
+                    index + 1, slide["choices"][i].get("answer"), position_list[i]))
